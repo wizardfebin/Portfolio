8115d537864feafd8d711e5edd91ee4bbe555a17
wizardfebin <febinmthw2000@gmail.com>
2024-10-10 14:32:53 +0530

    complete  responsiveness

--- script.js
+++ script.js
@@ -65,4 +65,12 @@ window.onscroll = function() {
     }
 };
 
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navList = document.querySelector('.nav-list');
+
+    hamburger.addEventListener('click', function() {
+        navList.classList.toggle('active'); // Toggle active class to show/hide menu
+    });
+});
 
